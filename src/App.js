1a82e6dbe5f09c4c52092d9021e49592af66d23d
Alexis <lxs.bnds@gmail.com>
2020-02-12 09:10:19 -0800

    navbar, wrapper, homepage files created, no content yet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as React from "react";
+import { ConfigProvider } from "./configContext";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+import Navbar from "./components/NavBar";
+import Wrapper from "./components/Wrapper";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="div">
+        <ConfigProvider>
+        <Router>
+        <Navbar/>
+        <Wrapper>
+        <Route exact path="/" component={Homepage}/>
+        </Wrapper>
+        </Router>
+        </ConfigProvider>
+      </div>
   );
 }
 
