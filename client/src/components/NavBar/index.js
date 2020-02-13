cbce049aa9efa4e483490713302e1fbb3eae0975
Alexis <lxs.bnds@gmail.com>
2020-02-13 13:36:29 -0800

    update my navbar or git gets it

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -8,38 +8,38 @@ const Navbar = props => {
         <Consumer>
             {context => {
                 return (
-                    <nav className="navbar navbar-light light-blue lighten-4">
-                      <a className="navbar-brand" href="#">Artiste Defammé</a>
-                    
-                      <button className="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1"
-                        aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"><span className="dark-blue-text"><i
-                            className="fas fa-bars fa-1x"></i></span></button>
-                    
-                      <div className="collapse navbar-collapse" id="navbarSupportedContent1">
-                    
-                        <ul className="navbar-nav mr-auto">
-                          <li className="nav-item active">
-                            <a className="nav-link" href="#">Dashboard <span className="sr-only">(current)</span></a>
-                          </li>
-                          <li className="nav-item">
-                            <a className="nav-link" href="#">Inventory</a>
-                          </li>
-                          <li className="nav-item">
-                            <a className="nav-link" href="#">Shop</a>
-                          </li>
-                          <li className="nav-item">
-                            <a className="nav-link" href="#">Studio</a>
-                          </li><li className="nav-item">
-                            <a className="nav-link" href="#">Gallery</a>
-                          </li>
-                          <li className="nav-item">
-                            <button>Sign Out</button>
-                          </li>
-                        </ul>
-                    
-                      </div>
-                    
-                    </nav>
+                  <nav className="navbar navbar-light light-blue lighten-4">
+                  <a className="navbar-brand" href="#">Artiste Defammé</a>
+                
+                  <button className="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1"
+                    aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"><span className="dark-blue-text"><i
+                        className="fas fa-bars fa-1x"></i></span></button>
+                
+                  <div className="collapse navbar-collapse" id="navbarSupportedContent1">
+                
+                    <ul className="navbar-nav mr-auto">
+                      <li className="nav-item active">
+                        <a className="nav-link" href="#">Dashboard <span className="sr-only">(current)</span></a>
+                      </li>
+                      <li className="nav-item">
+                        <a className="nav-link" href="#">Inventory</a>
+                      </li>
+                      <li className="nav-item">
+                        <a className="nav-link" href="#">Shop</a>
+                      </li>
+                      <li className="nav-item">
+                        <a className="nav-link" href="#">Studio</a>
+                      </li><li className="nav-item">
+                        <a className="nav-link" href="#">Gallery</a>
+                      </li>
+                      <li className="nav-item">
+                        <button>Sign Out</button>
+                      </li>
+                    </ul>
+                
+                  </div>
+                
+                </nav>
                 )
             }}
 
