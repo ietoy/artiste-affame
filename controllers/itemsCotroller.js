8e87d28056face11775e9a108b17866d1664ef5b
kqarlos <kqarlos@hotmail.com>
2020-02-14 12:48:46 -0800

    Adds user, painting, and items controllers

---
+++ controllers/itemsCotroller.js
@@ -0,0 +1,29 @@
+const db = require("../models");
+
+module.exports = {
+    //get Item by ID
+    findById: function (req, res) {
+        db.Item.findById({ _id: req.params.id })
+            .then(item => res.json(item))
+            .catch(err => console.log(err));
+    },
+    //Insert new item
+    create: function (req, res) {
+        db.Item.create(req.body)
+            .then(item => res.json(item))
+            .catch(err => res.status(422).json(err));
+    },
+    //update item
+    update: function (req, res) {
+        db.Item.findOneAndUpdate({ _id: req.params.id }, req.body)
+            .then(item => res.json(item))
+            .catch(err => res.status(422).json(err));
+    },
+    //remove item
+    remove: function (req, res) {
+        db.Item.findById({ _id: req.params.id })
+            .then(item => item.remove())
+            .then(item => res.json(item))
+            .catch(err => res.status(422).json(err));
+    }
+};
---
+++ controllers/usersController.js
@@ -0,0 +1,14 @@
+const db = require("../models");
+
+// Defining methods for the productsController
+module.exports = {
+  signUp: function(req, res) {
+    console.log(req.body);
+    db.User.create(req.body)
+      .then(data => res.json(data))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+    }
+};
