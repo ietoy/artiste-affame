ffde27bb8cebc6128eaf8386426f7b30177a1151
Ian Toy <iet@ela.com>
2020-02-18 15:55:12 -0800

    placeholder commit to save component work before pulling from master

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,19 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Navbar from "./components/NavBar";
 import Wrapper from "./components/Wrapper";
+
+// PAGES
 import Homepage from "./pages/Homepage";
-import Studio from "./pages/Studio"
+import Arcade from "./pages/Arcade";
+
+// PAINTINGS
+import Studio from "./pages/Studio";
+import Gallery from "./pages/Gallery";
+
+// ITEMS
+import Inventory from "./pages/Inventory";
+import Store from "./pages/Store";
+import Cart from "./pages/Cart";
 
 // WRITE A handleLike FUNCTION TO INCREMENT THE LIKE COUNT OF PAINTING OBJS +1 WHEN A BUTTON IS LIKED
 
@@ -13,13 +24,23 @@ function App() {
   return (
       <div className="div">
         <ConfigProvider>
-        <Router>
-        <Navbar/>
-        <Wrapper>
-            <Route exact path="/" component={Homepage} />
-            <Route exact path="/studio" component={Studio}/>
-        </Wrapper>
-        </Router>
+          <Router>
+            <Navbar/>
+            <Wrapper>
+                
+                <Route exact path="/" component={Homepage} />
+                <Route exact path="/arcade" component={Arcade}/>
+                
+                {/* Routes involving Paintings */}
+                <Route exact path="/studio" component={Studio}/>
+                <Route exact path="/gallery" component={Gallery}/>
+
+                {/* Routes involving Items */}
+                <Route exact path="/inventory" component={Inventory}/>
+                <Route exact path="/store" component={Store}/>
+                <Route exact path="/cart" component={Cart}/>
+            </Wrapper>
+          </Router>
         </ConfigProvider>
       </div>
   );
--- client/src/pages/Store/Store.js
+++ client/src/pages/Store/Store.js
@@ -1,6 +1,10 @@
 // IMPORT REACT
 import React from "react";
 
+// This page displays all items available for sale in the store
+// The user can view these items, add them to their shopping cart, and view their cart
+// The user can "checkout" from the cart page
+
 const Store = () => {
 
 }
