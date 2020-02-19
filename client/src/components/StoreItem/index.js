84f0cbee107fa29e7ec0bf91af9669893bc45186
Ian Toy <iet@ela.com>
2020-02-19 11:13:35 -0800

    allowed props to hanlde icon class when creating buttons

--- client/src/components/StoreItem/index.js
+++ client/src/components/StoreItem/index.js
@@ -6,8 +6,7 @@ const StoreItem = (props) => {
     return (
         <div>
             <a class="waves-effect waves-light btn">
-                {/* figure out how to concat in item name when generating icon */}
-                <i class="fas fa-cheese left"></i>
+                <i class={props.itemIcon}></i>
                     {props.itemName}
                 <i class="fas fa-dollar-sign right"></i>
                     {props.itemCost}                
