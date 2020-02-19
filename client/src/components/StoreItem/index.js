61a8a8f617d8406068eb3dffc340aa43e6c698c2
Ian Toy <iet@ela.com>
2020-02-19 10:43:44 -0800

    added font awesome link for icons to index.html

--- client/src/components/StoreItem/index.js
+++ client/src/components/StoreItem/index.js
@@ -1,7 +1,16 @@
 // IMPORT REACT
 import React from "react";
 
-const StoreItem = () => {
+const StoreItem = (props) => {
+
+    return (
+        <div>
+            <a class="btn-floating btn-large waves-effect waves-light red">
+                {/* <i class="material-icons">add</i> */}
+                <i class="fas fa-cheese">cheese</i>
+            </a>
+        </div>
+    )
 
 }
 
--- client/src/pages/Store/index.js
+++ client/src/pages/Store/index.js
@@ -1,11 +1,37 @@
 // IMPORT REACT
 import React from "react";
+import Consumer from "../../configContext.js";
+import StoreItem from "../../components/StoreItem/index.js"
 
 // This page displays all items available for sale in the store
 // The user can view these items, add them to their shopping cart, and view their cart
 // The user can "checkout" from the cart page
 
 const Store = () => {
+
+    // Return some JSX
+    return(
+        // Provides context to the data we intake here
+        <Consumer>
+            {context =>
+                {
+                    // Return some JSX
+                    return(
+                        context.marketplace_items.map(item => (
+                            <StoreItem
+                                // pass item name, icon if we have it
+                                // and the onclick function to add to cart
+                                // via passing props HERE
+                            />
+                        ))
+                    )
+                }
+                
+            }
+
+        </Consumer>
+
+    )
 
 }
 
