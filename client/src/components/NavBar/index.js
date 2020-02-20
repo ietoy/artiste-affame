055f9e97a7884af52caf79ee83138c8267a32edb
kqarlos <kqarlos@hotmail.com>
2020-02-20 12:35:51 -0800

    Updated NAvbar logic

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -6,56 +6,59 @@ import { Link } from "react-router-dom";
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 const Navbar = props => {
 
-  const getNav = (loggedin, context) => {
-    if (loggedin) {
-      return (
-        <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li> <Link to="/arcade">Arcade</Link>
-          </li>
-          <li> <Link to="/inventory">Inventory</Link>
-          </li>
-          <li>
-            <Link to="/shop">Shop</Link>
-          </li>
-          <li>
-            <Link to="/studio">Studio</Link>
-          </li><li>
-            <Link to="/gallery">Gallery</Link>
-          </li>
-          <li>
-            <Link to="/" onClick={() => context.logout()}>Sign Out</Link>
-          </li>
-        </ul>
-      )
-    }
-    else {
-      return (
-        <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li> <Link to="/arcade">Arcade</Link>
-          </li>
-          <li>
-            <Link to="/shop">Shop</Link>
-          </li>
-          <li>
-            <Link to="/studio">Studio</Link>
-          </li><li>
-            <Link to="/gallery">Gallery</Link>
-          </li>
-          <li>
-            <Link to="/login" >Sign In</Link>
-          </li>
-        </ul>
-      )
-    }
-  }
   return (
     <Consumer>
       {context => {
+
+        function getNav()  {
+          if (context.loggedIn) {
+            return (
+              <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li> <Link to="/arcade">Arcade</Link>
+                </li>
+                <li> <Link to="/inventory">Inventory</Link>
+                </li>
+                <li>
+                  <Link to="/shop">Shop</Link>
+                </li>
+                <li>
+                  <Link to="/studio">Studio</Link>
+                </li><li>
+                  <Link to="/gallery">Gallery</Link>
+                </li>
+                <li>
+                  <Link to="/" onClick={() => context.logout()}>Sign Out</Link>
+                </li>
+              </ul>
+            )
+          }
+          else {
+            return (
+              <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li> <Link to="/arcade">Arcade</Link>
+                </li>
+                <li>
+                  <Link to="/shop">Shop</Link>
+                </li>
+                <li>
+                  <Link to="/studio">Studio</Link>
+                </li><li>
+                  <Link to="/gallery">Gallery</Link>
+                </li>
+                <li>
+                  <Link to="/login" >Sign In</Link>
+                </li>
+              </ul>
+            )
+          }
+        }
+
+
         return (
           <nav>
             <div className="nav-wrapper">
               <Link to="/" className="brand-logo">Artiste Defammé</Link>
-              {getNav(context.loggedIn, context)}
+              {getNav()}
 
 
             </div>
