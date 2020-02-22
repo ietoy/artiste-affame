2af0adffe21b19dcb3a88d636ca8b74d131de4e5
Alexis <lxs.bnds@gmail.com>
2020-02-21 22:50:09 -0800

    modal is functional very ugly

--- client/src/pages/Studio/index.js
+++ client/src/pages/Studio/index.js
@@ -3,6 +3,8 @@ import Canvas from "../../components/Canvas";
 import ColorPalette from '../../components/ColorPalette';
 import Consumer from '../../configContext';
 import API from '../../utils/API';
+import axios from 'axios';
+import Modal from "../../components/Modal";
 
 function Studio() {
 
@@ -11,87 +13,105 @@ function Studio() {
     });
 
 
-    function changeColor(color){
+    function changeColor(color) {
         setStudioState({ strokeColor: color });
     }
-
-    function changeHandler(event){
-
-        console.log(event.target.files[0])
-    
+//need these for modal functionality
+    const toggleModalOn = () => {
+        setModalState({ isOpen: true});
     }
+    const toggleModalOff = () => {
+        setModalState({ isOpen: false});
+    }
+    const [modalState, setModalState] = useState({
+        isOpen: false,
+        onClose: toggleModalOff,
+
+    })
+  
     return (
-<Consumer>
-{context => {
-    // let widget =window.cloudinary.createUploadWidget({
-    
-    //     cloudName: "artiste-defamme",
-    //     uploadPreset: "f2ahbjhi"},
-    //     (error, result)=>{
-    //         if (result.info.files != undefined && context.loggedIn ===true){
-    //             // console.log(result.info.files[0]);
-    //             let painting = {
-    //                 painter: context.currentUser._id,
-    //                 name: result.info.files[0].name,
-    //                 src: result.info.files[0].uploadInfo.url,
-    //             }
-    //             console.log(painting);
-    //             API.addPainting(painting, context.currentUser._id).then( res => {
-    //                 // console.log("\n\n", res);
-    //             })
+        <Consumer>
+            {context => {
+                function changeHandler(event) {
+                    event.preventDefault();
+                    if (!context.loggedIn) {
+                        //toggling the modal on
+                        toggleModalOn();
+                    } else {
+                        var formData = new FormData();
+                        formData.append("file", event.target.files[0]);
+                        formData.append("upload_preset", "f2ahbjhi");
+                        formData.append("api_key", "878159185894491");
+                        formData.append("timestamp", (Date.now() / 1000) | 0);
+                        axios.post("https://api.cloudinary.com/v1_1/artiste-defamme/image/upload", formData, {
+                            headers: { "X-Requested-With": "XMLHttpRequest" },
+                        }).then(response => {
+                            const data = response.data;
+                            const fileURL = data.secure_url;
 
-    //         }
-    //         else if (error){
-    //             // console.log("I AM AN ERROR", error)
-    //         }
-           
-    //     }
-    // )
+                            let painting = {
+                                painter: context.currentUser._id,
+                                name: response.data.original_filename,
+                                src: fileURL
+                            }
+                            API.addPainting(painting, context.currentUser._id).then(res => {
+                                console.log("thisistheres", res)
+                            }).catch(err => {
 
-    // function showWidget(widget){
-    //     widget.open()
-    // }
-  
-
-return(
-
+                            })
+                        })
+                    }
+                }
 
 
-    <div className="container grey lighten-2">
-    <Fragment>
-        <h3 className="display-4" style={{ textAlign: 'center' }}> <i className="fas fa-brush"></i> Express yourself <i className="fas fa-brush"></i></h3>
+
+                return (
 
 
-        <div className="my-5 row">
 
-            <div className="col s2 color-guide">
-                <div className="" ><i className="fas fa-palette"></i> Color Guide</div>
-
-                <ColorPalette changeColor={changeColor}/>
-
-            </div>
-
-            <div className="container col s10">
-                <Canvas strokeColor={studioState.strokeColor} />
-            </div>
-
-        </div>
-
-    </Fragment>
-    <div>
-    <input name="file" type="file" class="cloudinary-fileupload" data-cloudinary-field="image_id" 
-   data-form-data="&quot;upload_preset&quot;:&quot;user_upload1&quot;" onChange={changeHandler}></input>
-    </div>
-</div>
+                    <div className="container grey lighten-2">
+                        <Fragment>
+                            <h3 className="display-4" style={{ textAlign: 'center' }}> <i className="fas fa-brush"></i> Express yourself <i className="fas fa-brush"></i></h3>
 
 
-)
+                            <div className="my-5 row">
 
-}}
+                                <div className="col s2 color-guide">
+                                    <div className="" ><i className="fas fa-palette"></i> Color Guide</div>
+
+                                    <ColorPalette changeColor={changeColor} />
+
+                                </div>
+
+                                <div className="container col s10">
+                                    <Canvas strokeColor={studioState.strokeColor} />
+                                </div>
+
+                            </div>
+                            {/* must pass these two props or it wont work, the children can make it unique, must go in fragment i think... */}
+                            <Modal show={modalState.isOpen}
+                            onClose={toggleModalOff}>
+                           <h1>what the heck</h1>
+                             </Modal>
+                     
+                           
+                        </Fragment>
+                  
+                        <div>
+                            <input name="file" type="file" class="cloudinary-fileupload" data-cloudinary-field="image_id"
+                                data-form-data="&quot;upload_preset&quot;:&quot;user_upload1&quot;" onChange={changeHandler}></input>
+                        </div>
+                    </div>
 
 
-</Consumer>
-  
+                )
+
+            }}
+      
+
+        </Consumer>
+
+
     );
 }
 
